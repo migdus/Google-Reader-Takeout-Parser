dc62a0b73410a3d40676a868b4ea6ec0d0cdca56
Miguel Dussán <migdusa@gmail.com>
2013-03-27 01:00:37 -0500

    XML works with bugs in tags.

--- parser.py
+++ parser.py
@@ -3,10 +3,15 @@ Created on Mar 26, 2013
 
 @author: migdus
 '''
-import zipfile,tempfile,Tkinter, tkFileDialog
-global_dict = {}
+import zipfile,tempfile,Tkinter, tkFileDialog, xml.etree.ElementTree as xml, os
+
+from datetime import datetime
+
 
 class parser:
+    global_dict = {}
+    find_zip_dict_key = 'zip_path'
+    find_dest_dict_key = 'destination_path' 
     
     def __init__(self,master):
         master.title('Quick n\' Dirty Google Reader Takeout Parser')
@@ -19,8 +24,8 @@ class parser:
         
         find_zip_entry_content=Tkinter.StringVar()
         select_file_text= 'Select a zip file'
-        find_zip_dict_key = 'zip_path'
-        self.buttonFindZip = Tkinter.Button(frame, text='Find zip',command=lambda:self.select_file_path(select_file_text,find_zip_dict_key,find_zip_entry_content))
+        
+        self.buttonFindZip = Tkinter.Button(frame, text='Find zip',command=lambda:self.select_file_path(select_file_text,self.find_zip_dict_key,find_zip_entry_content))
         self.buttonFindZip.grid(row=1,column=0)
         
         self.zip_path_field=Tkinter.Entry(frame, textvariable=find_zip_entry_content)
@@ -29,11 +34,11 @@ class parser:
         
         find_dir_entry_content=Tkinter.StringVar()
         select_dir_text = 'Select a directory'
-        find_dest_dict_key = 'destination_path' 
-        self.buttonDest = Tkinter.Button(frame,text='Destination',command=lambda:self.select_dir_path(select_dir_text, find_dest_dict_key,find_dir_entry_content))
+       
+        self.buttonDest = Tkinter.Button(frame,text='Destination',command=lambda:self.select_dir_path(select_dir_text, self.find_dest_dict_key,find_dir_entry_content))
         self.buttonDest.grid(row=2,column=0)
         
-        self.dest_field=Tkinter.Entry(frame, textvariable=lambda:global_dict[find_dest_dict_key])
+        self.dest_field=Tkinter.Entry(frame, textvariable=lambda:self.global_dict[self.find_dest_dict_key])
         self.dest_field.config(state=Tkinter.DISABLED)
         self.dest_field.grid(row=2,column=1)
         
@@ -44,22 +49,56 @@ class parser:
         plain_checked = Tkinter.IntVar()
         self.checkbox_plain = Tkinter.Checkbutton(frame, text="Plain text", variable=plain_checked)
         self.checkbox_plain.grid(row=3,column=1)
+        
+        self.buttonStart = Tkinter.Button(frame,text='Start Parsing',command=lambda:self.start_parsing(csv_checked,plain_checked))
+        self.buttonStart.grid(row=4,column=0,columnspan=2)
 
+        self.scrollbar = Tkinter.Scrollbar(frame)
+        self.scrollbar.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
+        self.scrollbar.grid(row=5,column=0,columnspan=2)
+
+        self.logbox = Tkinter.Listbox(frame, yscrollcommand=self.scrollbar.set)
+        self.logbox.grid(row=5,column=0,columnspan=2)
+        
+        self.logbox.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.logbox.yview)
+        
+        self.gui_logger("Program Started")
+
+    def gui_logger(self,message):
+        self.logbox.insert(Tkinter.END, datetime.now().isoformat()) 
+        self.logbox.insert(Tkinter.END, message)
+    
+    def start_parsing(self,csv_checked,plain_checked):
+        self.gui_logger("Starting")
+        if(csv_checked.get() == 1):
+            self.gui_logger("CSV Option selected")
+            
+        if plain_checked.get() == 1:
+            self.gui_logger("Plain Text Option selected")
+        
+        td = self.unzip(self.global_dict[self.find_zip_dict_key])
+        
+        files=self.search_path_files(td)
+        
+        for f in files:
+            fileName, fileExtension = os.path.splitext(f)
+            if fileExtension == '.xml': self.parse_xml(f)
+            
+ 
     def select_dir_path(self,window_title,dict_key,entry_content):
         root = Tkinter.Tk()
         root.withdraw()
         dirname = tkFileDialog.askdirectory(parent=root,initialdir="/",title=window_title)
-        global_dict[dict_key]=dirname
+        self.global_dict[dict_key]=dirname
         entry_content.set(dirname)
-        return
     
     def select_file_path(self,window_title,dict_key,entry_content):
         root = Tkinter.Tk()
         root.withdraw()
         file_name=tkFileDialog.askopenfilename(parent=root,initialdir="/",title=window_title)
-        global_dict[dict_key] = file_name
+        self.global_dict[dict_key] = file_name
         entry_content.set(file_name)
-        return
     
     def unzip(self,source):
         td = tempfile.mkdtemp()
@@ -67,9 +106,46 @@ class parser:
             for member in src.infolist():
                 src.extract(member,td)
         return td
+    
+    def search_path_files(self,td):
+        files=[]
+        full_dir_path = ''
+        for (dirpath, dirname, filenames) in os.walk(td):
+            files.extend(filenames)
+            full_dir_path=dirpath
+            if len(files)>0:
+                break
+        for i in range(0,len(files)):
+            files[i]=os.path.join(full_dir_path,files[i])
+        return files
+        
+            
+        
+        
+    def parse_xml(self,xml_path):
+        tree = xml.parse(xml_path)
+        rootElement =tree.getroot()
+        entries = {}
+        tag=''
+        for element in rootElement.iter('outline'):
+            if element.attrib.keys() == ['text','title']:
+                tag=element.attrib['title']
+            else: 
+                if element.attrib.keys() == ['xmlUrl','text','type','htmlUrl','title']:
+                    if element.attrib['xmlUrl'] not in entries.keys():
+                        if not element.attrib.has_key('tag'):
+                            element.attrib['tag']=[]
+                        
+                        if not tag in element.attrib['tag']:
+                            element.attrib['tag'].append(tag)
+                            
+                        entries[element.attrib['xmlUrl']]=element.attrib
+ 
+            self.gui_logger(element.attrib.keys())
 
 
-root =Tk()
+
+root =Tkinter.Tk()
 app=parser(root)
 root.mainloop()
 
