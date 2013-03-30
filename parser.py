d18c2e86531633f81a21ffa77a26a078c2a1f93a
Miguel Dussán <migdusa@gmail.com>
2013-03-30 11:51:26 -0500

    Naming conventions added.
    Instructions removed from window.

--- parser.py
+++ parser.py
@@ -23,28 +23,28 @@ class parser:
     def __init__(self, master):
         self.dest_filename = 'google_takeout_parsed'
         
-        self.global_dict = {}
-        self.find_zip_dict_key = 'zip_path'
-        self.find_dest_dict_key = 'destination_path' 
+        self.path_dict = {}
+        self.FIND_ZIP_DICT_KEY = 'zip_path'
+        self.FIND_DEST_DICT_KEY = 'destination_path' 
         
         
         self.widget_dict = {}
-        self.main_csv_key = 'main_csv'
-        self.csv_first_line_header_key = 'header_csv'
+        self.MAIN_CSV_WIDGET_KEY = 'main_csv'
+        self.CSV_FIRST_LINE_HEADER_KEY = 'header_csv'
     
         master.title('Quick n\' Dirty Google Reader Takeout Parser')
         frame = Tkinter.Frame(master)
         frame.grid()
         
-        textLabel = '1. Select your zip File.\n2. Check how would you like your info parsed.\n3.Select parsed files destination.\n4. Done!'
-        self.mainLabel = Tkinter.Label(frame, text=textLabel)
-        self.mainLabel.grid(row=0, column=0, columnspan=2)
-        
         find_zip_entry_content = Tkinter.StringVar()
         select_file_text = 'Select a zip file'
         
-        self.buttonFindZip = Tkinter.Button(frame, text='Find zip', command=lambda:self.select_file_path(select_file_text, self.find_zip_dict_key, find_zip_entry_content))
-        self.buttonFindZip.grid(row=1, column=0)
+        self.find_zip_button = Tkinter.Button(frame, text='Find zip', 
+                                            command=lambda:self.select_file_path(
+                                                                                 select_file_text, 
+                                                                                 self.FIND_ZIP_DICT_KEY, 
+                                                                                 find_zip_entry_content))
+        self.find_zip_button.grid(row=1, column=0)
         
         self.zip_path_field = Tkinter.Entry(frame, textvariable=find_zip_entry_content)
         self.zip_path_field.config(state=Tkinter.DISABLED)
@@ -53,13 +53,13 @@ class parser:
         find_dir_entry_content = Tkinter.StringVar()
         select_dir_text = 'Select a directory'
        
-        self.buttonDest = Tkinter.Button(frame, text='Destination', command=lambda:self.select_dir_path(
+        self.destination_button = Tkinter.Button(frame, text='Destination', command=lambda:self.select_dir_path(
                                                                                                         select_dir_text, 
-                                                                                                        self.find_dest_dict_key, 
+                                                                                                        self.FIND_DEST_DICT_KEY, 
                                                                                                         find_dir_entry_content))
-        self.buttonDest.grid(row=2, column=0)
+        self.destination_button.grid(row=2, column=0)
         
-        self.dest_field = Tkinter.Entry(frame, textvariable=lambda:self.global_dict[self.find_dest_dict_key])
+        self.dest_field = Tkinter.Entry(frame, textvariable=lambda:self.path_dict[self.FIND_DEST_DICT_KEY])
         self.dest_field.config(state=Tkinter.DISABLED)
         self.dest_field.grid(row=2, column=1)
         
@@ -67,27 +67,27 @@ class parser:
         self.checkbox_csv_first_line_header = Tkinter.Checkbutton(frame, text="1st line as column name",variable=csv_first_line_header)
         self.checkbox_csv_first_line_header.configure(state='disabled')
         self.checkbox_csv_first_line_header.grid(row=4, column=0)
-        self.widget_dict[self.csv_first_line_header_key] = self.checkbox_csv_first_line_header
+        self.widget_dict[self.CSV_FIRST_LINE_HEADER_KEY] = self.checkbox_csv_first_line_header
         
         csv_checked = Tkinter.IntVar()
         self.checkbox_csv = Tkinter.Checkbutton(frame, text="CSV", variable=csv_checked,
                                                 command=lambda:self.csv_checkbox_state(
                                                                                        csv_checked,
-                                                                                       self.csv_first_line_header_key,                                                                                       
+                                                                                       self.CSV_FIRST_LINE_HEADER_KEY,                                                                                       
                                                                                        csv_first_line_header))
         self.checkbox_csv.grid(row=3, column=0)
-        self.widget_dict[self.main_csv_key] = self.checkbox_csv
+        self.widget_dict[self.MAIN_CSV_WIDGET_KEY] = self.checkbox_csv
         
         plain_checked = Tkinter.IntVar()
         self.checkbox_plain = Tkinter.Checkbutton(frame, text="Plain text", variable=plain_checked)
         self.checkbox_plain.grid(row=3, column=1)
 
         
-        self.buttonStart = Tkinter.Button(frame, text='Start Parsing', command=lambda:self.start_parsing(find_dir_entry_content,
+        self.start_button = Tkinter.Button(frame, text='Start Parsing', command=lambda:self.start_parsing(find_dir_entry_content,
                                                                                                          csv_checked,
                                                                                                          csv_first_line_header, 
                                                                                                          plain_checked))
-        self.buttonStart.grid(row=5, column=0, columnspan=2)
+        self.start_button.grid(row=5, column=0, columnspan=2)
 
     def csv_checkbox_state(self,variable,child_key,child_key_var):
         if variable.get() == 0:
@@ -96,20 +96,10 @@ class parser:
         if variable.get() == 1:
             self.widget_dict[child_key].configure(state='normal')
             child_key_var.set(1)
-        
-    def gui_logger(self, message):
-        self.logbox.insert(Tkinter.END, datetime.now().isoformat()) 
-        self.logbox.insert(Tkinter.END, message)
     
     def start_parsing(self, dest_path,csv_checked, csv_first_line_header,plain_checked):
-        self.gui_logger("Starting")
-        if(csv_checked.get() == 1):
-            self.gui_logger("CSV Option selected")
-            
-        if plain_checked.get() == 1:
-            self.gui_logger("Plain Text Option selected")
         
-        td = self.unzip(self.global_dict[self.find_zip_dict_key])
+        td = self.unzip(self.path_dict[self.FIND_ZIP_DICT_KEY])
         
         files = self.search_path_files(td)
         
@@ -119,14 +109,14 @@ class parser:
                 if(csv_checked.get() == 1):
                         flag_header_written = False
                         with open(os.path.join(dest_path.get(),self.dest_filename+'.csv'),'wb') as csvfile:
-                            csvwriter=csv.writer(csvfile,delimiter=',',quotechar='"',quoting=csv.QUOTE_ALL)
+                            csv_writer=csv.writer(csvfile,delimiter=',',quotechar='"',quoting=csv.QUOTE_ALL)
                             
                             parsedxml = listparser.parse(f)
                             
                             keys=parsedxml.feeds[0].keys()
                             
                             if(csv_first_line_header.get() == 1 and not flag_header_written):
-                                csvwriter.writerow(keys)
+                                csv_writer.writerow(keys)
                                 flag_header_written = True
 
                             for e in parsedxml.feeds:
@@ -136,15 +126,11 @@ class parser:
                                         row.append(self.list_to_string(e[k]))
                                     else:
                                         row.append(e[k])
-                                csvwriter.writerow(row)
-                    
-                if plain_checked.get() == 1:
-                    self.gui_logger("Plain Text Option selected")
-                    self.gui_logger(e)
-                
+                                csv_writer.writerow(row)
+
     def list_to_string(self,l):
         
-        stringlist = ""
+        string_list = ""
         
         if len(l)>0:
             tempstr=''
@@ -152,22 +138,22 @@ class parser:
                 if type(ele) is list:
                     ele=self.list_to_string(ele)
                 tempstr+=ele+','
-                stringlist=tempstr[:-1]
-        return stringlist
+                string_list=tempstr[:-1]
+        return string_list
 
  
     def select_dir_path(self, window_title, dict_key, entry_content):
         root = Tkinter.Tk()
         root.withdraw()
-        dirname = tkFileDialog.askdirectory(parent=root, initialdir="/", title=window_title)
-        self.global_dict[dict_key] = dirname
-        entry_content.set(dirname)
+        dir_name = tkFileDialog.askdirectory(parent=root, initialdir="/", title=window_title)
+        self.path_dict[dict_key] = dir_name
+        entry_content.set(dir_name)
     
     def select_file_path(self, window_title, dict_key, entry_content):
         root = Tkinter.Tk()
         root.withdraw()
         file_name = tkFileDialog.askopenfilename(parent=root, initialdir="/", title=window_title)
-        self.global_dict[dict_key] = file_name
+        self.path_dict[dict_key] = file_name
         entry_content.set(file_name)
     
     def unzip(self, source):
@@ -208,9 +194,6 @@ class parser:
                             element.attrib['tag'].append(tag)
                             
                         entries[element.attrib['xmlUrl']] = element.attrib
-
-
-
 
 root = Tkinter.Tk()
 app = parser(root)
